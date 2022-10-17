edd71a1f267cd3c534dba2472bdb31f1c127c897
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-10-17 22:32:50 +0300

    initialized app using expo cli

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <Text>
+    Hello world
+   </Text>
+  {/* restaurant item*/}
+
+  {/* <View style={styles.restaurantContainer}>
+        <Image source={{ uri:'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg'}}
+        style={styles.image}/>
+  </View> */}
     </View>
   );
 }
@@ -17,4 +24,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  restaurantContainer:{
+    width:"100%",
+
+  },
+  image:{
+    width:"100%",
+    aspectRatio:5/3,
+  }
 });
